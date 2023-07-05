49bccb7d64ec7c976aeaba3c5717dbabeedddc6e
Nick <n.m.phelan@gmail.com>
2023-07-05 14:56:14 +0100

    Uploading a dodgy function, will remove later

--- script.js
+++ script.js
@@ -8,3 +8,17 @@ function handlePositiveNegative(event) {
     }
 }
 
+/* function handleBiggestNumber(event) {
+    const num_1 = event.target.value
+    const num_2 = event.target.value
+    const num_3 = event.target.value
+
+    if (num_1 > num_2 && num_1 > num_3) {
+        alert(num_1 + " is the biggest number!");
+    } else if (num_2 > num_1 && num_2 > num_3) {
+        alert(num_2 + " is the biggest number!")
+    } else {
+        alert(num_3 + " is the biggest number!")
+    }
+}  */ 
+
