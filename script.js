abc4fdf5d9e03fdc9fc428777cdb4f7e05d50b5b
Nick <n.m.phelan@gmail.com>
2023-07-05 14:26:29 +0100

    Added some HTML, and JS

---
+++ script.js
@@ -0,0 +1,10 @@
+function handlePositiveNegative(event) {
+    const num = event.target.value;
+
+    if (num > 0) {
+        alert("It's POSITIVE!")
+    } else {
+        alert("It's NEGATIVE!")
+    }
+}
+
