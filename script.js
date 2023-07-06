b67f0008a219d5a8cae19a7f43c5d47ae0c40152
Nick <n.m.phelan@gmail.com>
2023-07-06 09:54:42 +0100

    Messing around trying to add a date.. failed

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
+/*
+let datetime = new Date();
+console.log(datetime);
+document.getElementById("time").textContent = datetime;
+
+function refreshTime() {
+    const timeDisplay = document.getElementById("time");
+    const dateString = new Date().toLocaleString();
+    const formattedString = dateString.replace(", ", " - ");
+    timeDisplay.textContent = formattedString;
+}
+    setInterval(refreshTime, 1000);
+*/
+
+// Displaying negative or positive when number is prompted
 function handlePositiveNegative(event) {
     const num = event.target.value;
 
